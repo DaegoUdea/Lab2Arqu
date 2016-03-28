bb042262425fcfff92fcddf92128efce3389e4cf
Diego <daegoudea@gmail.com>
2016-03-28 13:07:41 -0500

    Merge origin/master
    Conflicts:
    	src/java/com/udea/controller/ClientServlet.java
    	src/java/com/udea/controller/VehicleServlet.java
    	web/clientInformation.jsp
    	web/index.jsp
    	web/vehicleInformation.jsp

---
+++ src/java/com/udea/logic/PaymentFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.logic;
+
+import com.udea.model.Payment;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Daego_000
+ */
+@Stateless
+public class PaymentFacade extends AbstractFacade<Payment> implements PaymentFacadeLocal {
+    @PersistenceContext(unitName = "Laboratorio2PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PaymentFacade() {
+        super(Payment.class);
+    }
+    
+}
